0af8ce540c83bae4044b6a8e40c5e230e6a337c7
yohan <noyohanx@naver.com>
2023-01-22 19:03:18 +0900

    :sparkles: : 유저모델 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(max_length=16, unique=True)
+    is_active = models.BooleanField(default=False)
+    first_name = None
+    last_name = None
+
+    def __str__(self) -> str:
+        return self.username
+
