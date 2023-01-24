12f9263afa6a5fa2cc0a4296426c54a097ce6382
yohan <noyohanx@naver.com>
2023-01-24 23:00:52 +0900

    writting 모델 추가

--- community/models.py
+++ community/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Writing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    text = models.TextField()
